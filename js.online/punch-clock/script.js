c44d5ae14101aa0ce8638457e9cc054bee173048
kessler yaron <koskasmail@gmail.com>
2023-03-31 09:34:16 +0000

    test

--- js.online/punch-clock/script.js
+++ js.online/punch-clock/script.js
@@ -20,18 +20,35 @@ var th = document.getElementById("endTime").value;
 }
 
 function diff(start, end) {
+    var output=[];
+
     start = start.split(":");
     end = end.split(":");
+
+    output.push(start);
+    output.push(end);
+
     var startDate = new Date(0, 0, 0, start[0], start[1], 0);
     var endDate = new Date(0, 0, 0, end[0], end[1], 0);
     var diff = endDate.getTime() - startDate.getTime();
+    
+    output.push(startDate);
+    output.push(endDate);
+    output.push(diff);
+
     var hours = Math.floor(diff / 1000 / 60 / 60);
     diff -= hours * 1000 * 60 * 60;
     var minutes = Math.floor(diff / 1000 / 60);
+    
+    output.push(diff);
+    output.push(hours);
+    output.push(minutes);
 
     // If using time pickers with 24 hours format, add the below line get exact hours
     if (hours < 0)
        hours = hours + 24;
+
+    console.table(output);
 
     return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
 }

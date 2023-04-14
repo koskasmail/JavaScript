2839ad768e855fa43a2f98713f8856b1839e9020
kessler yaron <koskasmail@gmail.com>
2023-04-14 13:39:54 +0000

    test

--- js.online/size/css_size/css_size.js
+++ js.online/size/css_size/css_size.js
@@ -1,7 +1,7 @@
 document.getElementById("pxInput").addEventListener("input", function() {
     var pxValue = parseFloat(this.value);
-    var emValue = pxValue / 16; // 1em = 16px
-    var remValue = pxValue / parseFloat(getComputedStyle(document.documentElement).fontSize); // 1rem = font-size of root element
+    var emValue = pxValue / 16;    //// (1em = 16px)
+    var remValue = pxValue / parseFloat(getComputedStyle(document.documentElement).fontSize); //// (1rem = font-size of root element)
 
     document.getElementById("emOutput").value = emValue.toFixed(2) + "em";
     document.getElementById("remOutput").value = remValue.toFixed(2) + "rem";

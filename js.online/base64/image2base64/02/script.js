079ec357731d20343c61eba90a43f40fcc88d257
kessler yaron <koskasmail@gmail.com>
2023-05-31 18:21:40 +0300

    ex

--- js.online/base64/image2base64/02/script.js
+++ js.online/base64/image2base64/02/script.js
@@ -1,1 +1,46 @@
-02
+
+/**
+ * 
+ */
+function convertToBase64() {
+  var input = document.getElementById('img');
+  var file = input.files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    var base64String = reader.result;
+    document.getElementById('output').innerText = base64String;
+    console.error(base64String);
+  };
+
+  reader.readAsDataURL(file);
+}
+
+/**
+ * 
+ */
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("output");
+  var status = document.getElementById("status");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  status.innerText = "copied to clipBoard !";
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  //alert("Copied to clipboard: " + copyText.value);
+}
+
+/**
+ * 
+ */
+function showBase64Image() {
+
+}
+

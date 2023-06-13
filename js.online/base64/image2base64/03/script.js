1f4e1925a6fcd9747a1247cbd281106a2f630677
kessler yaron <koskasmail@gmail.com>
2023-06-13 17:15:41 +0300

    ex
    check validation

---
+++ js.online/base64/image2base64/03/script.js
@@ -0,0 +1,44 @@
+const obj = {
+  _base64: '', 
+  get base64() {
+    return this._base64;
+  },
+  set base64(newbase64) {
+    this._base64 = newbase64;
+  }
+};
+
+function convertToBase64() {
+  var input = document.getElementById('img');
+  var file = input.files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    var base = reader.result;
+    base = reader.result;
+    document.getElementById('output').innerText = base;
+
+    ///console.log("obj.base64 >>> " + obj.base64);
+    obj.base64 = base; 
+    ///console.log("obj.base64 >>> " + obj.base64);
+    ///console.log("base: " + base);
+  };
+  reader.readAsDataURL(file);
+}
+
+function copyToClipboard() {
+
+  var copyText = document.getElementById("output");
+  var status = document.getElementById("status");
+  var imageOutput = document.getElementById("imageOutput");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  document.execCommand("copy");
+
+  status.innerText = "copied to clipBoard !";
+  ///console.log(">>> obj.base64 >>> " + obj.base64);
+
+  document.getElementById("imgOutput").src = obj.base64;
+}
